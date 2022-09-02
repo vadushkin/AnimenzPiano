6604708f7c61ae99e65b3fc0855a1c1db315ea5c
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-09-02 15:10:14 +0300

    added urls and about

--- sheets/urls.py
+++ sheets/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    # main page
+    path('', views.home, name='home'),
+
+    # contact
+    path('about/', views.about, name='about'),
+    # path('donate/', views.donate, name='donate'),
+    #
+    # # content
+    # path('archives/', views.archives, name='archives'),
+    # path('post/<slug:name>', views.post, name='post'),
+    # path('categories/uncategorized', views.uncategorized, name='uncategorized'),
+    # path('tags/', views.tags, name='tags'),
+    # path('tags/<slug:name>', views.tag_name, name='tag_name'),
 ]
--- sheets/views.py
+++ sheets/views.py
@@ -5,6 +5,14 @@ from .models import Sheet
 
 def home(request):
     context = {
-        'sheet': Sheet.objects.all()
+        'sheet': Sheet.objects.all(),
+        'title': 'Animenz 曲谱',
     }
     return render(request, 'sheets/index.html', context=context)
+
+
+def about(request):
+    context = {
+        'title': 'About',
+    }
+    return render(request, 'sheets/about.html', context=context)
