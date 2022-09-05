c155fb0a7507fdfeb8910f476b47f27f32bbecd3
Vadim <vadimsalapugin@gmail.com>
2022-09-05 18:46:37 +0300

    Update settings.py

--- animenz/settings.py
+++ animenz/settings.py
@@ -1,13 +1,9 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-p4hldk)!^n4(rrrzlz_^-c%!g$v9$a8e)7kobrlz6!qvtk#7gb'
 
 DEBUG = True
 
