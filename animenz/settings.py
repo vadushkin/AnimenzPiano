dab05b0822b4f6eeb3449d310f64b20534a1f9b6
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-09-05 20:53:30 +0300

    update

--- animenz/settings.py
+++ animenz/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-p4hldk)!^n4(rrrzlz_^-c%!g$v9$a8e)7kobrlz6!qvtk#7gb'
 
 DEBUG = True
 
