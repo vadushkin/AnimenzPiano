d06c8e65075a5a90396fe79938f0adb1a3d75a46
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-09-11 13:29:01 +0300

    added darkmode

---
+++ sheets/static/js/style.js
@@ -0,0 +1,26 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle = document.querySelector('#φda');
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
